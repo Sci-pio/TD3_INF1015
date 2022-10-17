ae582c0a972d3b3222f4032f5c12323bec62758d
Kamil Maarite <kamil.maarite@polymtl.ca>
2022-10-17 15:46:33 -0400

    petits changements

--- Liste.hpp
+++ Liste.hpp
@@ -15,39 +15,46 @@ using namespace iter;
 template<typename T>
 class Liste {
 public:
-	Liste() = default;
+	Liste();
 	//~Liste();
 
-	void ajouter(unique_ptr<T> element); 
+	void ajouter(shared_ptr<T> ptrT); 
+
 
 
 private:
-	std::size_t nElements_ = 0, capacite_ = 0;
-	unique_ptr<T> elements_ = nullptr;
+	std::size_t nElements_, capacite_;
+	unique_ptr<T> elements_[nElements_];
 
 	void changerCapacite(size_t nouvelleCapacite);
 };
 
 
 template<typename T>
-void Liste<T>:: ajouter(unique_ptr<T> element) {
+Liste<T>::Liste()
+{
+	nElements_ = 0; capacite_ = 0; elements_ = {};
+}
+
+template<typename T>
+void Liste<T>:: ajouter(shared_ptr<T> ptrT) {
 	if (nElements_ == capacite_)
 		changerCapacite(max(size_t(1), capacite_ * 2));
-	elements_[nElements_++] = element;
+	elements_[nElements_++] = ptrT;
 
 	// code temporaire de test:
-	for (size_t i : range(nElements_)) { cout << elements_[i]; }
-	cout << endl;
-	cout << capacite_ << endl;
-	cout << nElements_ << endl;
+	//for (size_t i : range(nElements_)) { cout << elements_[i]; }
+	//cout << endl;
+	//cout << capacite_ << endl;
+	//cout << nElements_ << endl;
 };
 
-// kamil: fonction necessaire pour pouvoir ajouter un element dans la liste
+
 template<typename T>
 void Liste<T>::changerCapacite(size_t nouvelleCapacite) {
 	assert(nouvelleCapacite >= nElements_); 
-	unique_ptr<T> nouvelleListe = shared_ptr<T> [nouvelleCapacite];
-	
+	unique_ptr<T> nouvelleListe = shared_ptr<T>[nouvelleCapacite];
+
 	for (size_t i : iter::range(nElements_))
 		nouvelleListe[i] = elements_[i];
 
