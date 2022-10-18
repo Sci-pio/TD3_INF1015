5de95528e9bb36ede0ab3cc4f893da2fd923791f
Kamil Maarite <kamil.maarite@polymtl.ca>
2022-10-18 16:43:26 -0400

    Changements Liste.hpp

--- Liste.hpp
+++ Liste.hpp
@@ -15,49 +15,48 @@ using namespace iter;
 template<typename T>
 class Liste {
 public:
-	Liste();
-	//~Liste();
+	Liste() = default;
+	~Liste();
 
-	void ajouter(shared_ptr<T> ptrT); 
+	void changerCapacite(size_t nouvelleCapacite) {
+		assert(nouvelleCapacite >= nElements_);
+		unique_ptr<T> nouvelleListe = shared_ptr<T>[nouvelleCapacite];
+
+		for (size_t i : iter::range(nElements_))
+			nouvelleListe[i] = elements_[i];
+
+		elements_ = nouvelleListe;
+		capacite_ = nouvelleCapacite;
+	};
+
+
+	void ajouter(shared_ptr<T> ptrT) {
+		if (nElements_ == capacite_)
+			changerCapacite(max(size_t(1), capacite_ * 2));
+		elements_[nElements_++] = ptrT;
+	};
+
+
+	void retirer(const shared_ptr<T> aRetirer) {
+		for (shared_ptr<T>& a : enSpan()) {
+			if (a==aRetirer){
+				if (nElements_ > 1)
+					a = elements_[nElements_ - 1];
+				nElements_--;
+			}
+		}
+	};
+
+	
+	span<T*> enSpan() const { return span(elements_, nElements_); };
+
 
 
 
 private:
-	std::size_t nElements_, capacite_;
-	unique_ptr<T> elements_[nElements_];
-
-	void changerCapacite(size_t nouvelleCapacite);
+	std::size_t nElements_ = 0, capacite_ = 0;
+	unique_ptr<shared_ptr<T>[]> elements_ = make_unique<shared_ptr<T>[]>(capacite_);
 };
 
 
-template<typename T>
-Liste<T>::Liste()
-{
-	nElements_ = 0; capacite_ = 0; elements_ = {};
-}
 
-template<typename T>
-void Liste<T>:: ajouter(shared_ptr<T> ptrT) {
-	if (nElements_ == capacite_)
-		changerCapacite(max(size_t(1), capacite_ * 2));
-	elements_[nElements_++] = ptrT;
-
-	// code temporaire de test:
-	//for (size_t i : range(nElements_)) { cout << elements_[i]; }
-	//cout << endl;
-	//cout << capacite_ << endl;
-	//cout << nElements_ << endl;
-};
-
-
-template<typename T>
-void Liste<T>::changerCapacite(size_t nouvelleCapacite) {
-	assert(nouvelleCapacite >= nElements_); 
-	unique_ptr<T> nouvelleListe = shared_ptr<T>[nouvelleCapacite];
-
-	for (size_t i : iter::range(nElements_))
-		nouvelleListe[i] = elements_[i];
-
-	elements_ = nouvelleListe;
-	capacite_ = nouvelleCapacite;
-}
